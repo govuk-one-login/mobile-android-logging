976912f7e71c01cd5216dd3c10742c975eade7b8
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2024-04-22 10:31:05 +0100

    build(workflows): Fix on_pull_request workflow
    Resolves: DCMAW-8361
    
    Co-authored-by: Aamir <160014668+aamirchoksi@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,4 @@
 buildscript {
-//    val minAndroidVersion by rootProject.extra { 29 }
-//    val compileAndroidVersion by rootProject.extra { 34 }
-//    val androidBuildToolsVersion by rootProject.extra { "34.0.0" }
-//    val composeKotlinCompilerVersion by rootProject.extra { "1.5.0" }
-//    val configDir by rootProject.extra { "$rootDir/config" }
-//    val baseNamespace by rootProject.extra { "uk.gov.android.ui" }
-
     repositories {
         google()
         gradlePluginPortal()
