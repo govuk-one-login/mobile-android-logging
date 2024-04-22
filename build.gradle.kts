247d027328210bffa7cc9611c7f00200960a4f88
Aamir Choksi <160014668+aamirchoksi@users.noreply.github.com>
2024-04-22 14:49:02 +0100

    wip(workflows): add sonarqube root config file
    Resolves: DCMAW-8361
    
    Co-authored-by: Jackson <152998610+JacksonJ3W@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
 
 plugins {
     id("uk.gov.logging.vale-config")
+    id("uk.gov.logging.sonarqube-root-config")
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
 }
