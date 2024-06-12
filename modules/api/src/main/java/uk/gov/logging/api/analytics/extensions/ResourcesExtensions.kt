c6bf2534ae4698cb2d5fdc051a2cba8cc255c72e
Jackson Wen <jackson.wen@digital.cabinet-office.gov.uk>
2024-06-12 12:10:55 +0100

    feat: Add functionality to get English plural res
    - Add `getEnQuantityString` to get English versions of quantity string
    resources regardless of device language, for analytics purposes. This
    was added to `ResourceExtensions.kt`
    
    Resolves: DCMAW-7001

--- modules/api/src/main/java/uk/gov/logging/api/analytics/extensions/ResourcesExtensions.kt
+++ modules/api/src/main/java/uk/gov/logging/api/analytics/extensions/ResourcesExtensions.kt
@@ -3,6 +3,7 @@ package uk.gov.logging.api.analytics.extensions
 import android.content.Context
 import android.content.res.Configuration
 import android.content.res.Resources
+import androidx.annotation.PluralsRes
 import androidx.annotation.StringRes
 import java.util.Locale
 
@@ -31,4 +32,32 @@ fun Context.getEnString(
     vararg formatArgs: Any
 ): String {
     return resources.getEnString(this, id, formatArgs)
+}
+
+@Suppress("AppBundleLocaleChanges")
+fun Resources.getEnQuantityString(
+    context: Context,
+    @PluralsRes
+    id: Int,
+    quantity: Int,
+    vararg formatArgs: Any
+): String {
+    val enConfig = Configuration(configuration)
+    enConfig.setLocale(Locale("en"))
+    val enResources = context.createConfigurationContext(enConfig).resources
+    return if (formatArgs.isEmpty()) {
+        enResources.getQuantityString(id, quantity)
+    } else {
+        enResources.getQuantityString(id, quantity, *formatArgs)
+    }
+}
+
+@Suppress("AppBundleLocaleChanges")
+fun Context.getEnQuantityString(
+    @PluralsRes
+    id: Int,
+    quantity: Int,
+    vararg formatArgs: Any
+): String {
+    return resources.getEnQuantityString(this, id, quantity, formatArgs)
 }
