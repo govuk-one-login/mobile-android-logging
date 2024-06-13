c8ce0beb8ff533746bf5d3c99706abfdca89fef0
Chuddington <6558350+Chuddington@users.noreply.github.com>
2024-06-13 15:15:01 +0100

    build(sonar): Create local scanning flow
    - Create bash scripts within `.sh/sonar` for handling the process:
      - `localProcess`: Completes the full local server scan, including
        cleaning the project and running the test suite. Be mindful of
        needing the `SONAR_TOKEN` environment variable.
      - `scan`: Move `uploadToSonar.sh` to be this file.
      - `localStart`: Defers to the `sonar` terminal command for starting
        the sonar server.
      - `localStop`: Defers to the `sonar` terminal command for stopping the
        sonar server.
      - `verifyPath`: Ensures that the `sonar` terminal command is in the
        PATH variable.

--- .sh/lintProject.sh
+++ .sh/lintProject.sh
@@ -14,7 +14,8 @@ TEST_RUNNER_MODE="$1"
 # The condition only checks whether the value has a non-zero length.
 SHOULD_RUN_DEPENDENCY_CHECKER="$2"
 
-shellcheck .sh/*
+# Lint all files in the .sh folder / sub-folders
+find ./.sh -type f -exec shellcheck {} +
 
 ./gradlew \
   :buildLogic:plugins:detekt \
