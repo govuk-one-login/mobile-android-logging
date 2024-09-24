c1ac91a011f4ef471171440cc8dff85894cca963
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-24 01:11:50 +0100

    feat: Update LinkParameters
    Update LinkParameters to have a set type parameter
    Update LinkParameters to simplify the asMap call
    Update LinkParametersTest to match
    Remove unused TypeGenericLink
    
    Resolves: 8381

--- modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/v2/LinkParameters.kt
+++ modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/v2/LinkParameters.kt
@@ -5,51 +5,41 @@ import uk.gov.logging.api.analytics.logging.HUNDRED_CHAR_LIMIT
 import uk.gov.logging.api.analytics.logging.LINK_DOMAIN
 import uk.gov.logging.api.analytics.logging.TEXT
 import uk.gov.logging.api.analytics.logging.TYPE
-import uk.gov.logging.api.analytics.parameters.ButtonParameters
 import uk.gov.logging.api.analytics.parameters.Mapper
-import uk.gov.logging.api.analytics.parameters.data.TypeGenericLink
+import uk.gov.logging.api.analytics.parameters.data.Type
 
 @Suppress("MaxLineLength")
 /**
- * Data class to contain the necessary values required for creating an Analytics event based on
- * navigating a User via a URL. Because of this, there is a need for both the [domain] and whether
- * the link is [external] or not.
+ * Data class to build the key value parameter pairs required for the TrackEventLink AnalyticsEvent.
+ * Use TrackEventLink to monitor the number of times users interact with links in the app.
+ * This includes all link type i.e. buttons and hyperlinked text.
  *
  * **see also:**
  * - https://govukverify.atlassian.net/wiki/spaces/PI/pages/3790995627/GA4+One+Login+Mobile+Application+Data+Schema+V3.1
  *
  * @param domain The domain of the accessed URL, such as `www.gov.uk`
- * @param external Whether the navigation is accessing a URL that's considered external
+ * @param `isExternal` Whether the navigation is accessing a URL that's considered external
  * @param text The lower case text displayed to the user of the hyperlink/cta that triggered
  * a navigation event
- * @param type Defaults to TypeGenericLink.GENERIC_LINK
  * @param overrides [Mapper] object that's applied after the setup specific to this
  * class
  *
- * @see ButtonParameters for the rest of the required parameters for a navigation event.
+ * @see RequiredParameters for the rest of the required parameters for a navigation event.
  */
 data class LinkParameters(
-    private val external: Boolean,
+    private val isExternal: Boolean,
     private val domain: String,
     private val text: String,
-    private val type: TypeGenericLink = TypeGenericLink.GENERIC_LINK,
     private val overrides: Mapper? = null
 ) : Mapper {
-    private val _linkDomain get() = domain.take(HUNDRED_CHAR_LIMIT).lowercase()
-    private val _external get() = external.toString().lowercase()
-    private val _text get() = text.take(HUNDRED_CHAR_LIMIT).lowercase()
+    private val _linkDomain get() = domain.take(HUNDRED_CHAR_LIMIT)
+    private val _isExternal get() = isExternal.toString()
+    private val _text get() = text.take(HUNDRED_CHAR_LIMIT)
 
-    override fun asMap(): Map<out String, Any?> {
-        val bundle = mutableMapOf<String, Any?>(
-            EXTERNAL to _external,
+    override fun asMap(): Map<out String, Any?> = mapOf<String, Any?>(
+            EXTERNAL to _isExternal,
             LINK_DOMAIN to _linkDomain,
             TEXT to _text,
-            TYPE to type.value,
-        )
-
-        val commonParameters: Map<out String, Any?> = overrides?.asMap() ?: mapOf()
-        bundle.putAll(commonParameters)
-
-        return bundle
-    }
+            TYPE to Type.Link.value,
+        ) + overrides?.asMap().orEmpty()
 }
--- modules/logging-api/src/test/java/uk/gov/logging/api/analytics/parameters/v2/LinkParametersTest.kt
+++ modules/logging-api/src/test/java/uk/gov/logging/api/analytics/parameters/v2/LinkParametersTest.kt
@@ -1,27 +1,51 @@
 package uk.gov.logging.api.analytics.parameters.v2
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import uk.gov.logging.api.analytics.logging.EXTERNAL
 import uk.gov.logging.api.analytics.logging.HUNDRED_CHAR_LIMIT
 import uk.gov.logging.api.analytics.logging.LINK_DOMAIN
+import uk.gov.logging.api.analytics.logging.TEXT
+import uk.gov.logging.api.analytics.logging.TYPE
 import uk.gov.logging.api.analytics.parameters.ParametersTestData
+import uk.gov.logging.api.analytics.parameters.data.Type
+import kotlin.test.assertContains
 
 class LinkParametersTest {
 
     private val exampleDomain = "www.unit.test"
 
     @Test
-    fun `Domain length is truncated to be 100 characters or less`() {
+    fun `text parameter value is truncated to be 100 characters or less`() {
+        // Given LinkParameters with text longer than 100 characters
+        val parameters = LinkParameters(
+            isExternal = false,
+            text = ParametersTestData.overOneHundredString,
+            domain = exampleDomain
+        )
+        val actual = parameters.asMap()[TEXT]
+        // Then the text value is truncated to be 100 characters or less
         assertEquals(
-            ParametersTestData.overOneHundredString.lowercase().take(HUNDRED_CHAR_LIMIT),
-            LinkParameters(
-                external = false,
-                text = "cta name",
-                domain = ParametersTestData.overOneHundredString
-            ).asMap().get(LINK_DOMAIN)
+            expected = ParametersTestData.overOneHundredString.take(HUNDRED_CHAR_LIMIT),
+            actual = actual
+        )
+    }
+
+    @Test
+    fun `Domain length is truncated to be 100 characters or less`() {
+        // Given LinkParameters with domain longer than 100 characters
+        val parameters = LinkParameters(
+            isExternal = false,
+            text = "cta name",
+            domain = ParametersTestData.overOneHundredString
+        )
+        val actual = parameters.asMap()[LINK_DOMAIN]
+        // Then the domain value is truncated to be 100 characters or less
+        assertEquals(
+            expected = ParametersTestData.overOneHundredString.take(HUNDRED_CHAR_LIMIT),
+            actual = actual
         )
     }
 
@@ -32,13 +56,14 @@ class LinkParametersTest {
     fun `Match output map`(isExternal: Boolean) {
         val expectedMap = mutableMapOf<String, Any?>(
             EXTERNAL to "$isExternal",
-            LINK_DOMAIN to exampleDomain.lowercase()
+            LINK_DOMAIN to exampleDomain.lowercase(),
+            TYPE to Type.Link.value
         )
 
         val mapper = LinkParameters(
             domain = exampleDomain,
             text = "cta name",
-            external = isExternal
+            isExternal = isExternal
         )
 
         val actual = mapper.asMap()
@@ -49,5 +74,23 @@ class LinkParametersTest {
                 actual[key]
             )
         }
+    }
+
+    @Test
+    fun `has required keys`() {
+        // Given LinkParameters
+        val parameters = LinkParameters(
+            isExternal = true,
+            domain = "domain.test",
+            text = "Test Button"
+        )
+        // Then both Text and Type parameters should be set
+        requiredKeys.forEach { expectedKey ->
+            assertContains(parameters.asMap().toMap(), expectedKey)
+        }
+    }
+
+    companion object {
+        private val requiredKeys = listOf(TEXT, TYPE, LINK_DOMAIN, EXTERNAL)
     }
 }
