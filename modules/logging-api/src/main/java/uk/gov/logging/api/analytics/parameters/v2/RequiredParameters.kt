46e6656de77bc155edb9fbef4a7180cc4c2a01a0
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-24 01:27:36 +0100

    feat: Update RequiredParameters
    Update RequiredParameters to remove the language parameter
    Update RequiredParametersTest to match
    
    Resolves: 8381

--- modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/v2/RequiredParameters.kt
+++ modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/v2/RequiredParameters.kt
@@ -1,8 +1,6 @@
 package uk.gov.logging.api.analytics.parameters.v2
 
 import androidx.annotation.CallSuper
-import java.util.Locale
-import uk.gov.logging.api.analytics.logging.LANGUAGE
 import uk.gov.logging.api.analytics.logging.ORGANISATION
 import uk.gov.logging.api.analytics.logging.PRIMARY_PUBLISHING_ORGANISATION
 import uk.gov.logging.api.analytics.logging.SAVED_DOC_TYPE
@@ -31,12 +29,9 @@ import uk.gov.logging.api.analytics.parameters.data.TaxonomyLevel3.UNDEFINED
  * - [TAXONOMY_LEVEL1], with the value of [taxonomyLevel1]. Default [ONE_LOGIN]
  * - [TAXONOMY_LEVEL2], with the value of [taxonomyLevel2].
  * - [TAXONOMY_LEVEL3], with the value of [taxonomyLevel3]. Default [UNDEFINED]
- * - [LANGUAGE], with the value of the default [Locale.getLanguage]. For english Users, this is
- *   `en`
  *
  * **see also:**
  *  - [GA4 | One Login Mobile Application Data Schema V3.1](https://govukverify.atlassian.net/wiki/x/qwD24Q)
- *
  */
 open class RequiredParameters(
     private val savedDocType: SavedDocType = SavedDocType.UNDEFINED,
@@ -48,13 +43,12 @@ open class RequiredParameters(
 ) : Mapper {
 
     @CallSuper
-    override fun asMap(): Map<out String, Any?> = mutableMapOf(
+    override fun asMap(): Map<out String, Any?> = mapOf(
         SAVED_DOC_TYPE to savedDocType.value,
         PRIMARY_PUBLISHING_ORGANISATION to primaryPublishingOrganisation.value,
         ORGANISATION to organisation.value,
         TAXONOMY_LEVEL1 to taxonomyLevel1.value,
         TAXONOMY_LEVEL2 to taxonomyLevel2.value,
-        TAXONOMY_LEVEL3 to taxonomyLevel3.value,
-        LANGUAGE to Locale.getDefault().language
+        TAXONOMY_LEVEL3 to taxonomyLevel3.value
     )
 }
