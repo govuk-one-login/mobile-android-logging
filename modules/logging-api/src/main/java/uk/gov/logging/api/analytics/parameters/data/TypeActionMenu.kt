48bd49bae1e4adcd39d4b4b5ad994d9713cd6817
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-09-24 01:01:49 +0100

    feat: Update ActionMenuParameters
    Update ActionMenuParameters to have a set type parameter
    Update ActionMenuParameters to simplify the asMap call
    Update ActionMenuParametersTest to match
    Remove unused TypeActionMenu
    
    Resolves: 8381

--- modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/data/TypeActionMenu.kt
+++
@@ -1,5 +0,0 @@
-package uk.gov.logging.api.analytics.parameters.data
-
-enum class TypeActionMenu(val value: String) {
-    ACTION_MENU("action menu"),
-}
--- modules/logging-api/src/test/java/uk/gov/logging/api/analytics/parameters/v2/ActionMenuParametersTest.kt
+++ modules/logging-api/src/test/java/uk/gov/logging/api/analytics/parameters/v2/ActionMenuParametersTest.kt
@@ -1,29 +1,33 @@
 package uk.gov.logging.api.analytics.parameters.v2
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.Test
 import uk.gov.logging.api.analytics.logging.HUNDRED_CHAR_LIMIT
 import uk.gov.logging.api.analytics.logging.TEXT
 import uk.gov.logging.api.analytics.logging.TYPE
 import uk.gov.logging.api.analytics.parameters.ParametersTestData
 import uk.gov.logging.api.analytics.parameters.data.TaxonomyLevel2
-import uk.gov.logging.api.analytics.parameters.data.TypeActionMenu
+import uk.gov.logging.api.analytics.parameters.data.Type
+import kotlin.test.assertContains
 
 class ActionMenuParametersTest {
 
     private val exampleCallToActionText = "button text"
 
     @Test
-    fun `text is truncated to be 100 characters or less`() {
+    fun `text parameter value is truncated to be 100 characters or less`() {
+        // Given ActionMenuParameters with text longer than 100 characters
+        val parameters = ActionMenuParameters(
+            text = ParametersTestData.overOneHundredString,
+            overrides = RequiredParameters(
+                taxonomyLevel2 = TaxonomyLevel2.GOVUK
+            )
+        )
+        val actual = parameters.asMap()[TEXT]
+        // Then the text value is truncated to be 100 characters or less
         assertEquals(
-            ParametersTestData.overOneHundredString.lowercase().take(HUNDRED_CHAR_LIMIT),
-            ActionMenuParameters(
-                text = ParametersTestData.overOneHundredString,
-                type = TypeActionMenu.ACTION_MENU,
-                overrides = RequiredParameters(
-                    taxonomyLevel2 = TaxonomyLevel2.GOVUK
-                )
-            ).asMap().get(TEXT)
+            expected = ParametersTestData.overOneHundredString.take(HUNDRED_CHAR_LIMIT),
+            actual = actual
         )
     }
 
@@ -31,12 +35,11 @@ class ActionMenuParametersTest {
     fun `Match output map`() {
         val expectedMap = mutableMapOf<String, Any?>(
             TEXT to exampleCallToActionText.lowercase(),
-            TYPE to TypeActionMenu.ACTION_MENU.value
+            TYPE to Type.ActionMenu.value
         )
 
         val mapper = ActionMenuParameters(
             text = exampleCallToActionText,
-            type = TypeActionMenu.ACTION_MENU,
             overrides = RequiredParameters(
                 taxonomyLevel2 = TaxonomyLevel2.GOVUK
             )
@@ -50,5 +53,21 @@ class ActionMenuParametersTest {
                 actual[key]
             )
         }
+    }
+
+    @Test
+    fun `has required keys`() {
+        // Given ActionMenuParameters
+        val parameters = ActionMenuParameters(
+            text = "Test Button"
+        )
+        // Then both Text and Type parameters should be set
+        requiredKeys.forEach { expectedKey ->
+            assertContains(parameters.asMap().toMap(), expectedKey)
+        }
+    }
+
+    companion object {
+        private val requiredKeys = listOf(TEXT, TYPE)
     }
 }
