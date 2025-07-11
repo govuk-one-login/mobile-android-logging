f70f8e17b1ee97acb1bf85b59cd62ded5f60cb46
Tom Ient <thomas.ient@digital.cabinet-office.gov.uk>
2025-07-11 17:36:32 +0100

    feat: DCMAW 14397 Update GA4 logging saved_doc_type  (#256)
    * feat: Put in real values for SavedDocType
    
    Resolves: 14397
    
    * feat: Update SavedDocType DRIVING_LICENCE value
    
    Resolves: 14397

--- modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/data/SavedDocType.kt
+++ modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/data/SavedDocType.kt
@@ -1,8 +1,9 @@
 package uk.gov.logging.api.analytics.parameters.data
 
 enum class SavedDocType(val value: String) {
-    PASSPORT("passport"),
-    BRP("brp"),
-    DRIVING_LICENCE("driving licence"),
+    DBS("dbs"),
+    NINO("nino"),
+    VETERANS_CARD("veterans card"),
+    DRIVING_LICENCE("mdl"),
     UNDEFINED("undefined"),
 }
