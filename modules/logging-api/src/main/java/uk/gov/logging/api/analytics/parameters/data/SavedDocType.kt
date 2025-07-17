68f53b0f1aa1d98606d8289dde548a4b8fdaaa73
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2025-07-17 11:57:09 +0100

    feat: Make saved_doc_type dynamic
    Update RequiredParameters to take a string for saved_doc_type
    Update AnalyticsLoggerConstants to have SAVED_DOC_TYPE_UNDEFINED
    Update RequiredParametersTest to verify truncated string
    Remove SavedDocType enum
    
    Resolves: 14397

--- modules/logging-api/src/main/java/uk/gov/logging/api/analytics/parameters/data/SavedDocType.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.logging.api.analytics.parameters.data
-
-enum class SavedDocType(val value: String) {
-    DBS("dbs"),
-    NINO("nino"),
-    VETERAN_CARD("veteran card"),
-    DRIVING_LICENCE("mdl"),
-    UNDEFINED("undefined"),
-}
