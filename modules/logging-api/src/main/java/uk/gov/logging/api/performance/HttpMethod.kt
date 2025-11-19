711f103325e4f6463eddc7deea52c830ef6d1bbb
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2025-11-19 18:45:00 +0000

    feat: Add HttpMethod unit test
    Resolves: 15747

--- modules/logging-api/src/main/java/uk/gov/logging/api/performance/HttpMethod.kt
+++ modules/logging-api/src/main/java/uk/gov/logging/api/performance/HttpMethod.kt
@@ -1,5 +1,11 @@
 package uk.gov.logging.api.performance
 
+/**
+ * Represents standard HTTP methods supported by Firebase Performance Monitoring.
+ *
+ * Using this enum ensures type safety and prevents errors when referencing
+ * FirebasePerformance.HttpMethod values.
+ */
 enum class HttpMethod {
     GET,
     POST,
