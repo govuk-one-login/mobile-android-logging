3d0b2683eb9c3c0715d521d09e10c088d73ebbe6
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2025-11-26 15:52:41 +0000

    fix: Move FakePerformanceMonitor to where it is used
    Resolves: 15747

--- modules/logging-api/src/test/java/uk/gov/logging/api/performance/MonitorHelpersTest.kt
+++ modules/logging-api/src/test/java/uk/gov/logging/api/performance/MonitorHelpersTest.kt
@@ -5,6 +5,7 @@ import org.mockito.Mockito.mock
 import org.mockito.kotlin.any
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
+import java.net.URL
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
@@ -56,4 +57,42 @@ class MonitorHelpersTest {
             verify(trace, times(0)).putAttribute(any(), any())
             assertTrue(isRun)
         }
+
+    private class FakePerformanceMonitor(
+        val trace: Trace = Trace(),
+        val httpMetric: HttpMetric = HttpMetric(),
+    ) : PerformanceMonitor {
+        override fun newTrace(name: String): PerformanceMonitor.Trace = trace
+
+        override fun newHTTPMetric(
+            url: URL,
+            method: HttpMethod,
+        ): PerformanceMonitor.HttpMetric = httpMetric
+
+        class Trace : PerformanceMonitor.Trace {
+            override fun putAttribute(
+                key: String,
+                value: String,
+            ) = Unit
+
+            override fun incrementMetric(
+                name: String,
+                by: Long,
+            ) = Unit
+
+            override fun stop() = Unit
+        }
+
+        class HttpMetric : PerformanceMonitor.HttpMetric {
+            override fun setRequestSize(bytes: Long) = Unit
+
+            override fun setResponseSize(bytes: Long) = Unit
+
+            override fun setResponseCode(code: Int) = Unit
+
+            override fun setContentType(type: String) = Unit
+
+            override fun stop() = Unit
+        }
+    }
 }
