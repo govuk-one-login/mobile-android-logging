c6b3d874481c11042f93bb25d1d12b6ab4ac4fa3
Tom Ient <thomas.ient@digital.cabinet-office.gov.uk>
2026-02-26 12:57:25 +0000

    fix: turn Crashlytics on by default (#332)
    Resolves: 18605

--- modules/logging-impl/src/main/java/uk/gov/logging/impl/analytics/extensions/SetCollectionEnabled.kt
+++ modules/logging-impl/src/main/java/uk/gov/logging/impl/analytics/extensions/SetCollectionEnabled.kt
@@ -7,10 +7,11 @@ import com.google.firebase.perf.performance
 
 /**
  * Enables / disables both Firebase analytics, crashlytics, and performance.
+ * Crashlytics is always allowed. It is included here to ensure any accidental disabling are undone
  * Needs updating when performance is allowed
  */
 fun Firebase.setCollectionEnabled(isEnabled: Boolean) {
     this.analytics.setAnalyticsCollectionEnabled(isEnabled)
-    this.crashlytics.isCrashlyticsCollectionEnabled = isEnabled
+    this.crashlytics.isCrashlyticsCollectionEnabled = true
     this.performance.isPerformanceCollectionEnabled = false
 }
