10fc6c739909765f34e27f3edd7fdc4ee162c204
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-03-12 09:38:33 +0000

    Fix: klintformat, detekt the PR

--- modules/logging-api/src/test/java/uk/gov/logging/api/v2/errorKeys/ErrorKeysTest.kt
+++ modules/logging-api/src/test/java/uk/gov/logging/api/v2/errorKeys/ErrorKeysTest.kt
@@ -3,12 +3,9 @@ package uk.gov.logging.api.v2.errorKeys
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
 
-
 internal class ErrorKeysTest {
-
     @Test
     fun `Test string error keys returns correct key and value`() {
-
         val stringErrorKey = ErrorKeys.StringKey(KEY, STRING_VALUE)
         assertTrue { stringErrorKey.key == KEY }
         assertTrue { stringErrorKey.value == STRING_VALUE }
@@ -22,31 +19,26 @@ internal class ErrorKeysTest {
     }
 
     @Test
-    fun `Test double error keys returns correct key and value`(){
-        val doubleErrorKey = ErrorKeys.DoubleKey(KEY,DOUBLE_VALUE)
+    fun `Test double error keys returns correct key and value`() {
+        val doubleErrorKey = ErrorKeys.DoubleKey(KEY, DOUBLE_VALUE)
         assertTrue { doubleErrorKey.key == KEY }
         assertTrue { doubleErrorKey.value == DOUBLE_VALUE }
-
     }
 
     @Test
-    fun `Test boolean error keys returns correct key and value`(){
-        val booleanErrorKey = ErrorKeys.BooleanKey(KEY,BOOLEAN_VALUE)
+    fun `Test boolean error keys returns correct key and value`() {
+        val booleanErrorKey = ErrorKeys.BooleanKey(KEY, BOOLEAN_VALUE)
         assertTrue { booleanErrorKey.key == KEY }
         assertTrue { booleanErrorKey.value }
-
     }
 
-
-
-    companion object{
+    companion object {
         private const val KEY = "key"
-        private const  val STRING_VALUE = "value"
-        private const val  INT_VALUE = 1
+        private const val STRING_VALUE = "value"
+        private const val INT_VALUE = 1
 
         private const val DOUBLE_VALUE = 1.0
 
         private const val BOOLEAN_VALUE = true
     }
-
 }
