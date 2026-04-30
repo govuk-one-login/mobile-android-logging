24e3b417318b9f6ae8c37c49c981114ecd2de499
Elijah Hezekiah <elijah.hezekiah@digital.cabinet-office.gov.uk>
2026-04-30 16:10:57 +0100

    fix: Logging package enhancements version two

--- modules/logging-api/src/main/java/uk/gov/logging/api/v3/LoggingProperties.kt
+++ modules/logging-api/src/main/java/uk/gov/logging/api/v3/LoggingProperties.kt
@@ -2,8 +2,12 @@ package uk.gov.logging.api.v3
 
 /**
  *  Logging properties data class use for
+ *   checking if [LogEntry] is to be logged remotely
+ *
+ *   @property allowRemote
  *
  */
+
 data class LoggingProperties(
     val allowRemote: Boolean = true,
 )
